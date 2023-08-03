867dd90c2ede95f5a37b17128a5b5a42492c139a
Peter Nowee <peter@peternowee.com>
2023-08-03 16:26:33 +0200

    Initial commit of count-service
    Microservice returning the count of triples
    
    Initial setup based on examples from:
    - redpencil.io Masterclass 2021Q1, 04 - Templates and conventions
      https://drive.google.com/drive/folders/1BYH_PsBiycorEzNtJvJWS4riIQIZCa0s
    - https://github.com/mu-semtech/mu-javascript-template/pull/50/files
      https://mu.semte.ch/2017/06/29/building-a-microservice-with-express/
    - https://github.com/mu-semtech/mu-javascript-template/blob/v1.6.0/README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# count-service
+
+Microservice returning the count of triples
---
+++ app.js
@@ -0,0 +1,16 @@
+import { app, query, errorHandler } from "mu";
+
+app.get('/', function( req, res ) {
+  res.send('Hello from count-service');
+} );
+
+app.get( "/count", async function( req, res ) {
+  const resp = await query( `
+    SELECT COUNT(*) WHERE {
+      GRAPH <http://mu.semte.ch/graphs/public> {
+        ?s ?p ?o. } }
+  ` );
+  res.send( JSON.stringify( resp ) );
+});
+
+app.use( errorHandler );
