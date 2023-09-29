c971168ce5b06200ed080ccba0b5bd484153a40e
Peter Nowee <peter@peternowee.com>
2023-09-29 17:33:22 +0200

    ESLint prettier fixes

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import { app, query, errorHandler } from "mu";
+import { app, query, errorHandler } from 'mu';
 
-app.get('/', function( req, res ) {
+app.get('/', function (req, res) {
   res.send('Hello from count-service');
-} );
+});
 
-app.get( "/count", async function( req, res ) {
-  const resp = await query( `
+app.get('/count', async function (req, res) {
+  const resp = await query(`
     SELECT (COUNT(*) AS ?triples) WHERE {
       GRAPH <http://mu.semte.ch/graphs/public> {
         ?s ?p ?o. } }
-  ` );
-  res.send( JSON.stringify( resp ) );
+  `);
+  res.send(JSON.stringify(resp));
 });
 
-app.use( errorHandler );
+app.use(errorHandler);
